79fbc4a655c1d47c7acce75eba71c3238e30aa62
alemi <me@alemi.dev>
2024-12-02 23:29:43 +0100

    feat: now with 100% more sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-
-use std::{collections::{HashMap, VecDeque}, sync::Arc};
+use std::{collections::HashMap, sync::Arc};
 
 use clap::Parser;
-use tokio::sync::RwLock;
+use rusqlite::{named_params, params, Connection, OptionalExtension};
+use tokio::sync::Mutex;
 
 #[derive(Parser)]
 struct Cli {
+	/// path to storage file, if not given will operate in memory
+	storage: Option<String>,
+
 	/// path to config file
-	#[arg(short, long, default_value = "uppe-rs.toml")]
+	#[arg(short, long, default_value = "uppe.toml")]
 	config: String,
 
 	/// host to bind api onto
@@ -21,7 +24,7 @@ struct Config {
 	service: std::collections::BTreeMap<String, Service>,
 
 	/// how many samples of history to keep
-	history: usize,
+	//history: usize,
 
 	/// poll services at this interval
 	interval_s: u64,
@@ -36,61 +39,6 @@ struct Service {
 	interval_s: Option<u64>,
 }
 
-type AppState = Arc<RwLock<StateStorage>>;
-
-type Event = (i64, Option<i64>);
-
-struct StateStorage {
-	size: usize,
-	store: HashMap<String, VecDeque<Event>>,
-}
-
-impl StateStorage {
-	fn new(size: usize) -> AppState {
-		Arc::new(RwLock::new(Self {
-			size, store: HashMap::default(),
-		}))
-	}
-
-	fn get(&self, k: &str) -> Vec<Event> {
-		match self.store.get(k) {
-			Some(x) => x.clone().into(),
-			None => Vec::new(),
-		}
-	}
-
-	fn put(&mut self, key: &str, timestamp: i64, rtt: Option<i64>) {
-		match self.store.get_mut(key) {
-			Some(x) => {
-				x.push_back((timestamp, rtt));
-				while x.len() > self.size {
-					x.pop_front();
-				}
-			},
-			None => {
-				let mut q = VecDeque::new();
-				q.push_back((timestamp, rtt));
-				self.store.insert(key.to_string(), q);
-			},
-		}
-	}
-
-	fn up(&self, key: &str) -> bool {
-		match self.store.get(key) {
-			None => false, // this key is not being tracked, or we never polled it
-			Some(x) => match x.back() {
-				None => false, // this key has never been polled yet
-				Some((_, None)) => false, // last poll was a failure
-				Some((_, Some(_))) => true, // last poll was a success
-			}
-		}
-	}
-
-	fn services(&self) -> Vec<String> {
-		self.store.keys().cloned().collect()
-	}
-}
-
 fn main() {
 	let cli = Cli::parse();
 
@@ -100,32 +48,37 @@ fn main() {
 	let config = toml::from_str::<Config>(&raw_config)
 		.expect("invalid config format");
 
+	let db =  Database::open(cli.storage.as_deref())
+		.expect("failed instantiating database");
+
 	tokio::runtime::Builder::new_current_thread()
 		.enable_all()
 		.build()
 		.expect("could not create tokio runtime")
-		.block_on(entry(cli, config))
+		.block_on(entry(cli, config, db))
 		.expect("event loop terminated with error");
 }
 
-async fn entry(cli: Cli, config: Config) -> Result<(), Box<dyn std::error::Error>> {
-	let state = StateStorage::new(config.history);
+async fn entry(cli: Cli, config: Config, db: Database) -> Result<(), Box<dyn std::error::Error>> {
 	let default_interval = config.interval_s;
 
 	for (key, service) in config.service {
 		let interval = service.interval_s.unwrap_or(default_interval);
-		let state = state.clone();
+		let db = db.clone();
+		let sid = db.sid(&key).await?;
 
 		tokio::spawn(async move {
 			loop {
 				let res = test(&service.endpoint).await;
-				let timestamp = chrono::Utc::now().timestamp();
-				match res {
-					Ok(rtt) => state.write().await.put(&key, timestamp, Some(rtt)),
+				let value = match res {
+					Ok(rtt) => Some(rtt),
 					Err(e) => {
-						eprintln!("[!] error polling service {key}: {e}");
-						state.write().await.put(&key, timestamp, None);
+						eprintln!(" ?  error polling service {key}: {e} -- {e:?}");
+						None
 					},
+				};
+				if let Err(e) = db.insert(sid, value).await {
+					eprintln!("[!] error inserting value in database: {e} -- {e:?}");
 				}
 				tokio::time::sleep(std::time::Duration::from_secs(interval)).await;
 			}
@@ -137,12 +90,39 @@ async fn entry(cli: Cli, config: Config) -> Result<(), Box<dyn std::error::Error
 		.route("/", axum::routing::get(root))
 		.route("/api/status", axum::routing::get(api_status))
 		.route("/api/status/:service", axum::routing::get(api_status_service))
-		.with_state(state);
+		.with_state(db);
 
 	let listener = tokio::net::TcpListener::bind(&cli.addr).await?;
 	axum::serve(listener, app).await?;
 
 	Ok(())
+}
+
+
+// ============= APIs
+
+
+type ApiResult<T> = Result<Json<T>, ApiError>;
+
+#[derive(Debug, thiserror::Error)]
+enum ApiError {
+	#[error("error interacting with database: {0}")]
+	Db(#[from] rusqlite::Error),
+}
+
+impl IntoResponse for ApiError {
+	fn into_response(self) -> axum::response::Response {
+		match self {
+			ApiError::Db(error) => (
+				axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+				Json(serde_json::json!({
+					"error": "database",
+					"message": format!("{error}"),
+					"struct": format!("{error:?}"),
+				}))
+			).into_response(),
+		}
+	}
 }
 
 async fn root() -> Html<&'static str> {
@@ -158,23 +138,147 @@ async fn test(url: &str) -> reqwest::Result<i64> {
 	Ok(delta.num_milliseconds())
 }
 
-use axum::{extract::{Path, State}, response::Html, Json};
+use axum::{extract::{Path, Query, State}, response::{Html, IntoResponse}, Json};
+
+#[derive(serde::Deserialize)]
+struct StatusQuery {
+	since: Option<i64>,
+}
 
 async fn api_status(
-	State(state): State<AppState>,
-) -> Json<HashMap<String, bool>> {
-	let services = state.read().await.services();
-	let mut out = HashMap::new();
-	for service in services {
-		let up = state.read().await.up(&service);
-		out.insert(service, up);
+	State(db): State<Database>,
+	Query(q): Query<StatusQuery>,
+) -> ApiResult<HashMap<String, Option<i64>>> {
+	let mut state = HashMap::new();
+	let five_min_ago = (chrono::Utc::now() - chrono::Duration::minutes(5)).timestamp();
+	let since = q.since.unwrap_or(five_min_ago);
+	for (sid, name) in db.services().await? {
+		state.insert(
+			name,
+			db.up(sid, since).await?
+		);
 	}
-	Json(out)
+	Ok(Json(state))
+}
+
+#[derive(serde::Deserialize)]
+struct ServiceStatusQuery {
+	limit: Option<i64>,
 }
 
 async fn api_status_service(
-	State(state): State<AppState>,
+	State(db): State<Database>,
 	Path(service): axum::extract::Path<String>,
-) -> Json<Vec<(i64, Option<i64>)>> {
-	Json(state.read().await.get(&service))
+	Query(q): Query<ServiceStatusQuery>,
+) -> ApiResult<Vec<(i64, Option<i64>)>> {
+	let limit = q.limit.unwrap_or(50).min(250);
+	let sid = db.sid(&service).await?;
+	Ok(Json(db.get(sid, Some(limit)).await?))
+}
+
+
+// ============= DATABASE
+
+
+type Event = (i64, Option<i64>);
+
+#[derive(Clone)]
+struct Database(Arc<Mutex<Connection>>);
+
+impl Database {
+	fn open(path: Option<&str>) -> rusqlite::Result<Self> {
+		let db = match path {
+			Some(p) => Connection::open(p)?,
+			None => Connection::open_in_memory()?,
+		};
+		db.execute(
+			"CREATE TABLE IF NOT EXISTS events (
+				id INTEGER PRIMARY KEY AUTO INCREMENT,
+				service INTEGER NOT NULL,
+				time BIG INTEGER NOT NULL,
+				value BIG INTEGER NULL,
+			)", params![]
+		)?;
+
+		db.execute(
+			"CREATE INDEX IF NOT EXISTS event_per_service
+			ON events (service)",
+			params![],
+		)?;
+	
+		db.execute(
+			"CREATE TABLE IF NOT EXISTS services (
+				id INTEGER PRIMARY KEY AUTO INCREMENT,
+				name STRING NOT NULL,
+			)", params![]
+		)?;
+
+		db.execute(
+			"CREATE INDEX IF NOT EXISTS services_names_lookup
+			ON services (name)",
+			params![],
+		)?;
+
+		Ok(Self(Arc::new(Mutex::new(db))))
+	}
+
+	async fn services(&self) -> rusqlite::Result<Vec<(i64, String)>> {
+		let db = self.0.lock().await;
+		let mut stmt = db.prepare("SELECT * FROM services")?;
+		let res = stmt.query_map(
+			params![],
+			|row| Ok((row.get(0)?, row.get(1)?))
+		)?;
+		Ok(res.filter_map(|x| x.ok()).collect())
+	}
+	
+	async fn insert(&self, sid: i64, value: Option<i64>) -> rusqlite::Result<()> {
+		self.0.lock().await.execute(
+			"INSERT INTO events(service, time, value) VALUES (:sid, :time, :value)",
+			named_params! { ":sid": sid, ":time": chrono::Utc::now().timestamp(), ":value": value }
+		)?;
+		
+		Ok(())
+	}
+
+	async fn get(&self, sid: i64, limit: Option<i64>) -> rusqlite::Result<Vec<Event>> {
+		let db = self.0.lock().await;
+		let mut stmt = db.prepare("SELECT time, value FROM events WHERE sid = :sid LIMIT :limit")?;
+		let results = stmt.query_map(
+			named_params! { ":sid": sid, ":limit": limit },
+			|row| Ok((row.get(0)?, row.get(1).optional()?)),
+		)?;
+
+		Ok(
+			results
+				.filter_map(|x| x.ok())
+				.collect()
+		)
+	}
+
+	#[async_recursion::async_recursion]
+	async fn sid(&self, service: &str) -> rusqlite::Result<i64> {
+		let res = {
+			let db = self.0.lock().await;
+			let mut stmt = db.prepare("SELECT id FROM services WHERE name = ?")?;
+			stmt.query_row(params![service], |row| row.get(0)).optional()?
+		};
+
+		match res {
+			Some(sid) => Ok(sid),
+			None => {
+				self.0.lock().await.execute("INSERT INTO services(name) VALUES ?", params![service])?;
+				self.sid(service).await
+			}
+		}
+	}
+
+	async fn up(&self, sid: i64, since: i64) -> rusqlite::Result<Option<i64>> {
+		let db = self.0.lock().await;
+		let mut stmt = db.prepare("SELECT value FROM events WHERE service = :sid AND time > :time")?;
+		stmt.query_row(
+			named_params! { ":sid": sid, ":time": since },
+			|row| row.get(0).optional()
+		)
+	}
 }
