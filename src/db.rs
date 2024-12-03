40ac1d825e8adef673ba90212a0dbb39c6aa03df
alemi <me@alemi.dev>
2024-12-03 14:41:55 +0100

    fix: limit comes after order by

--- src/db.rs
+++ src/db.rs
@@ -67,7 +67,7 @@ impl Database {
 
 	pub async fn get(&self, sid: i64, limit: i64) -> rusqlite::Result<Vec<Event>> {
 		let db = self.0.lock().await;
-		let mut stmt = db.prepare("SELECT time, value FROM events WHERE service = :sid LIMIT :limit ORDER BY time DESC")?;
+		let mut stmt = db.prepare("SELECT time, value FROM events WHERE service = :sid ORDER BY time DESC LIMIT :limit")?;
 		let results = stmt.query_map(
 			named_params! { ":sid": sid, ":limit": limit },
 			|row| Ok((row.get(0)?, row.get(1)?)),
